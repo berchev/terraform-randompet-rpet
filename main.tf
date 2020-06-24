728c7282f5fedbbc7895bae6d63362d300b52260
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2020-06-24 08:04:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,26 @@
 
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "georgiman"
+
+    workspaces {
+      name = "test-rm-terraform"
+    }
+  }
+}
 
 resource "random_pet" "name" {
+ length    = "8"
+ separator = "-"
+}
+
+resource "random_pet" "name1" {
+ length    = "8"
+ separator = "-"
+}
+
+resource "random_pet" "name2" {
  length    = "8"
  separator = "-"
 }
